ceb11309aecab3a18bb6129b8406acb412f321b6
Jakub Romanowski <58742151+GGBJacob@users.noreply.github.com>
2024-10-26 10:37:37 +0200

    v 1.1
    - Better logs
    - More instructions in the README

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 A bash script pushing all files within a specified path to a git repository.
 
-## Running
+## Task Configuration
 
 ### Step 1:
 
@@ -20,6 +20,13 @@ Add a trigger that suits your needs.
 
 Enter cmd.exe as desired Program/Script and pass "/c start "" /min "path\to\my\file.bat" ^& exit" as arguments.
 
-### Step 5:
+## Script Configuration
 
-Enjoy!
+
+### Step 1:
+
+Set the FOLDER variable to your local repository location.
+
+### Step 2:
+
+Set the LIMIT variable to skip pushing files above x bytes. 
--- script.bat
+++ script.bat
@@ -1,54 +1,57 @@
 @echo off
 setlocal enabledelayedexpansion
 
-
+REM TEST
 
 REM Path to repo folder
-set FOLDER=X:\Desktop\Test automatyzacji
+set FOLDER="path\to\your\file.bat"
 cd /d "%FOLDER%"
 
-echo. > logs.txt
+set LOG=latest.log
+
+echo %date% %time% > %LOG%
 
 REM Check if any changes were made
 git diff-index --quiet HEAD
 IF %ERRORLEVEL% EQU 0 (
+    echo No changes to commit. >> %LOG%
     exit
 )
+(
+    REM File size limit
+    set LIMIT=30000000
 
-REM File size limit
-set LIMIT=30000000
+    REM File iteration (adding only those below limit)
+    for /r %%i in (*.*) do (
+        set "currentPath=%%~dpi"
+        set "currentPath=!currentPath:\=\\!"
 
-REM File iteration (adding only those below limit)
-for /r %%i in (*.*) do (
-    set "currentPath=%%~dpi"
-    set "currentPath=!currentPath:\=\\!"
-
-    if "!currentPath!" neq "!currentPath:.git=!" (
-        echo Skipping %%i - .git content >> logs.txt
-    ) else (
-        set "file=%%i"
-        set size=0
-        for %%s in (%%~zi) do set size=%%s
-
-        REM Check size
-        if !size! LSS !LIMIT! (
-            echo Adding: !file! >> logs.txt
-            git add "!file!"
+        if "!currentPath!" neq "!currentPath:.git=!" (
+            echo Skipping %%i - .git content
         ) else (
-            echo Skipping !file! - file size exceeds !LIMIT! bytes >> logs.txt
+            set "file=%%i"
+            set size=0
+            for %%s in (%%~zi) do set size=%%s
+
+            REM Check size
+            if !size! LSS !LIMIT! (
+                echo Adding: !file!
+                git add "!file!"
+            ) else (
+                echo Skipping !file! - file size exceeds !LIMIT! bytes
+            )
         )
     )
-)
 
-REM Fetch all deleted files and remove them from the repo
-for /f "delims=" %%j in ('git ls-files --deleted') do (
-    echo Removing from repo: %%j >> logs.txt
-    git rm "%%j"
-)
+    REM Fetch all deleted files and remove them from the repo
+    for /f "delims=" %%j in ('git ls-files --deleted') do (
+        echo Removing from repo: %%j
+        git rm "%%j"
+    )
 
-REM Finalize everything
-git commit -m "Automated commit: %date% %time%" >> logs.txt 2>&1
-git push origin main >> logs.txt 2>&1
-
+    REM Finalize everything
+    git commit -m "Automated commit: %date% %time%"
+    git push origin main
+) >> %LOG% 2>&1
 REM Finish
 exit
