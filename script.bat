47386fbdea77e08eba29c142823db27fe6276963
Jakub Romanowski <58742151+GGBJacob@users.noreply.github.com>
2024-11-02 23:25:51 +0100

    Major code changes
    Removed file size limit (temporarily)
    Shortened code by a lot

--- script.bat
+++ script.bat
@@ -19,62 +19,15 @@ IF %ERRORLEVEL% EQU 0 (
     exit
 )
 
-REM Clear logs prior to commiting
-echo. > %LOG%
-
 (
-    REM File size limit (30 MB)
-    set LIMIT=30000000
-    
-    echo Scanning dirs...
-    echo.
-    
-    REM Iterate through directories excluding .git (faster than skipping all files within .git)
-    for /D %%d in (*) do (
-        if /i not "%%~nd"==".git" (
-            REM Recursive file iteration in non-.git directories
-            for /r "%%d" %%i in (*.*) do (
-                set "file=%%i"
-                set size=0
-                for %%s in (%%~zi) do set size=%%s
-
-                REM Check size
-                if !size! LSS !LIMIT! (
-                    echo Adding: !file!
-                    git add "!file!"
-                ) else (
-                    echo Skipping !file! - file size exceeds !LIMIT! bytes
-                )
-            )
-        ) else (
-            echo Skipping .git directory: %%d
-        )
-    )
-
-    echo.
-    echo Scanning files...
-    echo.
-
-    REM Iterate through files only in main directory
-    for %%i in (*.*) do (
-            set "file=%%i"
-            set size=0
-            for %%s in (%%~zi) do set size=%%s
-
-            REM Check size
-            if !size! LSS !LIMIT! (
-                echo Adding: !file!
-                git add "!file!"
-            ) else (
-                echo Skipping !file! - file size exceeds !LIMIT! bytes
-            )
-        )
-    
+    git add -A
     REM Fetch all deleted files and remove them from the repo
     for /f "delims=" %%j in ('git ls-files --deleted') do (
         echo Removing from repo: %%j
         git rm "%%j"
     )
+
+    git status --porcelain
 
     echo.
     echo Committing...
@@ -84,7 +37,7 @@ echo. > %LOG%
     git commit -m "Automated commit: %date% %time%"
     git push origin main
 
-) >> %LOG% 2>&1
+) > %LOG% 2>&1
 
 REM Push logs to github (add logs to .gitignore to prevent [and run git rm -r --cached if already commited])
 git add %LOG%
